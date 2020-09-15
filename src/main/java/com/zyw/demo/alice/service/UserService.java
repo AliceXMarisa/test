53c50bbfa08fdcb4cb387fe5424aaef27e5ebb03
AliceXMarisa <452918608@qq.com>
2020-09-15 17:48:24 +0800

    test

---
+++ src/main/java/com/zyw/demo/alice/service/UserService.java
@@ -0,0 +1,16 @@
+package com.zyw.demo.alice.service;
+
+import com.zyw.demo.alice.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zyw
+ * @since 2019-09-24
+ */
+public interface UserService extends IService<User> {
+
+}
